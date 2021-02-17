eeebed8a9e9ff813229c571da45a90919bc3b915
aaiZaeN <max.concept89@gmail.com>
2021-02-17 14:31:37 +0100

    Header completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.scss';
 import 'bootstrap/scss/bootstrap.scss'
+import Particles from 'react-particles-js'
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
   return (
-   <Navbar/>
+  <>
+    <Particles
+      className="particles-canvas"
+      params={{
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 900,
+            }
+          },
+          shape: {
+            type: 'square',
+            stroke: {
+              width: 6,
+              color: '#f9ab00'
+            }
+          }
+        }
+      }}
+    />
+    <Navbar/>
+    <Header/>
+  </>  
   );
 }
 
