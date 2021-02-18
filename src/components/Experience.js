c6c9852ecd74d8b2caf81364077e7917b412fb25
aaiZaeN <max.concept89@gmail.com>
2021-02-18 14:45:22 +0100

    Experience completed

---
+++ src/components/Experience.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Experience = () => {
+      return (
+            <div className="experience">
+                  <div className="d-flex justify-content-center my-5">
+                        <h1>experience</h1>
+                  </div>
+                  <div className="container experienceWrapper">
+                        <div className="timeline-block timeline-block-right">
+                              <div className="marker"></div>
+                              <div className="timeline-content">
+                                    <h3>2014-2015</h3>
+                                    <p>Et excepteur nulla labore dolore enim officia dolor ex quis ipsum voluptate deserunt. Pariatur voluptate labore esse mollit laboris. Deserunt eu consectetur laboris magna aliqua sint ad cillum voluptate incididunt incididunt.</p>
+                              </div>
+                        </div>
+                        {/* - */}
+                        <div className="timeline-block timeline-block-left">
+                              <div className="marker"></div>
+                              <div className="timeline-content">
+                                    <h3>2014-2015</h3>
+                                    <p>Et excepteur nulla labore dolore enim officia dolor ex quis ipsum voluptate deserunt. Pariatur voluptate labore esse mollit laboris. Deserunt eu consectetur laboris magna aliqua sint ad cillum voluptate incididunt incididunt.</p>
+                              </div>
+                        </div>
+                        {/* - */}
+                        <div className="timeline-block timeline-block-right">
+                              <div className="marker"></div>
+                              <div className="timeline-content">
+                                    <h3>2014-2015</h3>
+                                    <p>Et excepteur nulla labore dolore enim officia dolor ex quis ipsum voluptate deserunt. Pariatur voluptate labore esse mollit laboris. Deserunt eu consectetur laboris magna aliqua sint ad cillum voluptate incididunt incididunt.</p>
+                              </div>
+                        </div>
+                        {/* - */}
+                        <div className="timeline-block timeline-block-left">
+                              <div className="marker"></div>
+                              <div className="timeline-content">
+                                    <h3>2014-2015</h3>
+                                    <p>Et excepteur nulla labore dolore enim officia dolor ex quis ipsum voluptate deserunt. Pariatur voluptate labore esse mollit laboris. Deserunt eu consectetur laboris magna aliqua sint ad cillum voluptate incididunt incididunt.</p>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      )
+}
+
+export default Experience
