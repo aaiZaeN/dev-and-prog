b3a757cbb483ef233db502415059c9ddb1938552
aaiZaeN <max.concept89@gmail.com>
2021-02-18 13:27:30 +0100

    services completed

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,7 +12,7 @@ const AboutMe = () => {
                         </div>
                         <div className="col-lg-6 col-xm-12">
                               <h1 className="aboutHeading">A propos</h1>
-                              <p>Depuis toujours attiré par les métiers du web, je tente l'expérience d'auto-entrepreneur à l'âge de 20 ans, sans être formé, je repars donc travailler dans divers emplois.
+                              <p className="text-justify">Depuis toujours attiré par les métiers du web, je tente l'expérience d'auto-entrepreneur à l'âge de 20 ans, sans être formé, je repars donc travailler dans divers emplois.
                                Après 18 mois chez Fruehauf, je découvre OpenClassRooms qui m'offre l'opportunité de me former au métier qui me passionne depuis toujours : Développeur Web. Après un moment de réflexion,
                                je décide de me lancer et de tenter ma chance. J'obtiens donc mon diplôme en novembre 2020..</p>
                         </div>
